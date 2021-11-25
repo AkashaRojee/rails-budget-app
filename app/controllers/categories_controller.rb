ad135dddd08706c7bacb11b100cb028d234b71c2
Akasha <Akasha.Rojee@gmail.com>
2021-11-25 17:38:14 +0400

    Add new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,9 +10,29 @@ class CategoriesController < ApplicationController
     @purchases = @category.purchases.order(created_at: 'desc')
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = current_user.categories.new(category_params)
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to root_path }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
   private
 
   def set_category
     @category = Category.includes(:purchases).find(params[:id])
+  end
+
+  def category_params
+    params.fetch(:category, {}).permit(:name, :icon)
   end
 end
