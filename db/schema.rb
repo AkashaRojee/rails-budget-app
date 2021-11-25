0bc4ba2280b2e040e3f3a157c2489d43caa4bd19
Akasha <Akasha.Rojee@gmail.com>
2021-11-25 15:50:26 +0400

    Add name to User

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_11_25_114410) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
