a81ee4f3f8072b6f14a339faecb404e651c1f3ec
Akasha <Akasha.Rojee@gmail.com>
2021-11-26 20:07:48 +0400

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, :email, presence: true, length: { maximum: 100 }
+  validates :email, uniqueness: true
+
   has_many :purchases
   has_many :categories
 end
